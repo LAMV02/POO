55a1dba10667903e2936d8e0e0babdf9f8686ba3
Luisitolink <80289761+Luisitolink@users.noreply.github.com>
2021-04-22 12:07:13 -0600

    Planetas pero con el get
    XD

--- Planetas/src/Asteroid.java
+++ Planetas/src/Asteroid.java
@@ -9,7 +9,19 @@ public class Asteroid {
         this.composition=composition;
         this.distance_of_earth=distance_of_earth;
         this.speed=speed;
+    }
+        public void setname(String nuevoNombre){
+            this.name=nuevoNombre;
+        }
+        public String getName(){
+        return  this.name;
+        }
 
+    public void setDistance_of_earth(double distance_of_earth) {
+        this.distance_of_earth = distance_of_earth;
     }
 
+    public double getDistance_of_earth(){
+        return this.distance_of_earth;
+    }
 }
